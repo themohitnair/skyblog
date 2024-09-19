0fdd39674805b2d9faa55ae0886a7bec933fdc87
Mohit <themohitnair@protonmail.com>
2024-09-19 22:36:30 +0530

    no js needed lol

--- app/endpoints.go
+++ app/endpoints.go
@@ -34,6 +34,24 @@ func Endpoints() *gin.Engine {
 
 	router.LoadHTMLGlob("templates/*")
 
+	router.GET("/", func(c *gin.Context) {
+		allMetadata, err := fileops.GetAllBlogMetadata()
+		if err != nil {
+			if strings.Contains(err.Error(), "no such file or directory") || len(allMetadata) == 0 {
+				c.HTML(200, "index.html", gin.H{
+					"Blogs": []fileops.BlogMetadata{},
+				})
+				return
+			}
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.HTML(200, "index.html", gin.H{
+			"Blogs": allMetadata,
+		})
+	})
+
 	router.GET("/:blogName", func(c *gin.Context) {
 		blogName := c.Param("blogName")
 		dirPath := fmt.Sprintf("./blogs/%s", blogName)
@@ -60,15 +78,9 @@ func Endpoints() *gin.Engine {
 		})
 	})
 
-	router.GET("/", func(c *gin.Context) {
-		allMetadata, err := fileops.GetAllBlogMetadata()
-		if err != nil {
-			c.JSON(500, gin.H{"error": err.Error()})
-			return
-		}
-
-		c.HTML(200, "index.html", gin.H{
-			"Blogs": allMetadata,
+	router.NoRoute(func(c *gin.Context) {
+		c.HTML(404, "404.html", gin.H{
+			"Message": "Oops! The page you are looking for does not exist.",
 		})
 	})
 	return router
